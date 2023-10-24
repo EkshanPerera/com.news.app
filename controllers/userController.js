9a97eb56db004e5da288abf103727ad7dfd4f22e
Ekshan Perera <ekshan.perera@gmail.com>
2023-10-24 19:59:48 +0530

    V 1.2

--- controllers/userController.js
+++ controllers/userController.js
@@ -57,7 +57,7 @@ const loginUser = asyncHandler(async (req, res) => {
           },
         },
         process.env.ACCESS_TOKEN_SECERT,
-        { expiresIn: "15m" }
+        { expiresIn: "1d" }
       );
       res.status(200).json({ accessToken });
     }else{
